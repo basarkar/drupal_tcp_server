04d468b2bdc6f8e87532890041095102309e8c01
bappa.sarkar <bappa.sarkar@262655.no-reply.drupal.org>
2014-06-26 16:33:26 +0530

    changing the config file.

--- drupal_config.php
+++ drupal_config.php
@@ -2,7 +2,13 @@
 $current_dir = getcwd();
 $drupal_path = "/var/www/html/d6";
 chdir($drupal_path);
+$_SERVER['HTTP_HOST'] = 'example.org';
+$_SERVER['REMOTE_ADDR'] = '127.0.0.1';
+define('DRUPAL_ROOT',$drupal_path);
+
+// Bootstrap.
 require_once './includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 
-
+global $user;
+$user = user_load(1);
